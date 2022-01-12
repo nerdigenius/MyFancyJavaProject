9cb2b9d773fb08fb7629b420e28f9834296c911c
nerdigenius <Ashiq.racer@gmail.com>
2022-01-13 00:05:28 +0600

    singleton redone

--- src/main/java/com/mycompany/myfancyjavaproject/Main.java
+++ src/main/java/com/mycompany/myfancyjavaproject/Main.java
@@ -8,6 +8,18 @@ package com.mycompany.myfancyjavaproject;
  *
  * @author Ashiq
  */
+public class Main {
+    public static void main(String[] args) {
+        Database db1;
+
+      // refers to the only object of Database
+      db1= Database.getInstance();
+      
+      db1.getConnection();
+    }
+   
+}
+
 class Database {
    private static Database dbObject;
 
@@ -30,13 +42,3 @@ class Database {
    }
 }
 
-class Main {
-   public static void main(String[] args) {
-      Database db1;
-
-      // refers to the only object of Database
-      db1= Database.getInstance();
-      
-      db1.getConnection();
-   }
-}
