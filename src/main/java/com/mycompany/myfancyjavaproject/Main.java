d9aae6d08e52e6c264884176d9f14fb65c6b1ef8
nerdigenius <Ashiq.racer@gmail.com>
2022-01-13 00:06:22 +0600

    Revert "Merge branch 'branchY' into branchXY"
    This reverts commit 4261bad526cafb71781259e322bc3c2ed3779b3b, reversing
    changes made to 3c77e12172c153e716e725096c2b3297fcd33136.

--- src/main/java/com/mycompany/myfancyjavaproject/Main.java
+++ src/main/java/com/mycompany/myfancyjavaproject/Main.java
@@ -14,25 +14,3 @@ public class Main {
     }
    
 }
-
-class Database {
-   private static Database dbObject;
-
-   private Database() {      
-   }
-
-   public static Database getInstance() {
-
-      // create object if it's not already created
-      if(dbObject == null) {
-         dbObject = new Database();
-      }
-
-       // returns the singleton object
-       return dbObject;
-   }
-
-   public void getConnection() {
-       System.out.println("You are now connected to the database.");
-   }
-}
